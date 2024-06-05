b4cd21db308a795874d3878b39d52d60b8d55ccc
Eduardoooxd <ecouto93@gmail.com>
2024-06-05 07:13:53 +0100

    fix: Fixing wrong name of import

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,7 +2,7 @@
 
 import EduardoImage from '@/assets/images/eduardo_couto.jpg';
 import { contactInformation } from '@/lib/data';
-import Image from 'Next/image';
+import Image from 'Next/Image';
 import { motion } from 'framer-motion';
 import { ArrowDownToLine, ArrowUpRight, Github, Linkedin } from 'lucide-react';
 import Link from 'next/link';
