f226055b3e1174451a4ff8221458d64ea109b3c3
Eduardoooxd <ecouto93@gmail.com>
2024-06-08 16:24:43 +0100

    fix: fix header bar only triggering once animation for experience section

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -1,13 +1,18 @@
 'use client';
 import { experiencesData } from '@/lib/data';
 import { useSectionInView } from '@/lib/hooks';
+import { useInView } from 'react-intersection-observer';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import SectionHeading from './section-heading';
 
 export default function Experience() {
-  const { ref, inView } = useSectionInView({
+  const { ref } = useSectionInView({
     sectionName: 'Experience',
+  });
+
+  const { ref: timelineInViewRef, inView: timelineInView } = useInView({
+    threshold: 0.3,
     triggerOnce: true,
   });
 
@@ -15,29 +20,31 @@ export default function Experience() {
     <section id="experience" ref={ref} className="scroll-mt-28">
       <SectionHeading>My Experience</SectionHeading>
 
-      <VerticalTimeline lineColor="">
-        {experiencesData.map(({ title, location, date, description, icon }, index) => (
-          <VerticalTimelineElement
-            key={index}
-            contentStyle={{
-              background: '#f3f4f6',
-              boxShadow: 'none',
-              border: '1px solid rgba(0, 0, 0, 0.05)',
-              textAlign: 'left',
-              padding: '1.3rem 2rem',
-            }}
-            contentArrowStyle={{ borderRight: '0.4rem solid #9ca3af' }}
-            date={date}
-            icon={icon}
-            iconStyle={{ background: 'white', fontSize: '1.5rem' }}
-            visible={inView}
-          >
-            <h3 className="font-semibold capitalize">{title}</h3>
-            <p className="!mt-0 font-normal">{location}</p>
-            <p className="!mt-1 !font-normal text-gray-700">{description}</p>
-          </VerticalTimelineElement>
-        ))}
-      </VerticalTimeline>
+      <div ref={timelineInViewRef}>
+        <VerticalTimeline lineColor="">
+          {experiencesData.map(({ title, location, date, description, icon }, index) => (
+            <VerticalTimelineElement
+              key={index}
+              contentStyle={{
+                background: '#f3f4f6',
+                boxShadow: 'none',
+                border: '1px solid rgba(0, 0, 0, 0.05)',
+                textAlign: 'left',
+                padding: '1.3rem 2rem',
+              }}
+              contentArrowStyle={{ borderRight: '0.4rem solid #9ca3af' }}
+              date={date}
+              icon={icon}
+              iconStyle={{ background: 'white', fontSize: '1.5rem' }}
+              visible={timelineInView}
+            >
+              <h3 className="font-semibold capitalize">{title}</h3>
+              <p className="!mt-0 font-normal">{location}</p>
+              <p className="!mt-1 !font-normal text-gray-700">{description}</p>
+            </VerticalTimelineElement>
+          ))}
+        </VerticalTimeline>
+      </div>
     </section>
   );
 }
