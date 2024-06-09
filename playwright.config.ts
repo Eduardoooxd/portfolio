565526200f61661f99472c5b3ecb82352e4c8f52
Eduardoooxd <ecouto93@gmail.com>
2024-06-09 23:44:12 +0100

    ci: adding playwright ctrf report to pipeline

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,13 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html', { outputDir: 'plawright-report', outputFile: 'report.html' }],
+    [
+      'playwright-ctrf-json-reporter',
+      { outputDir: 'playwright-report', outputFile: 'ctrf-report.json' },
+    ],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
