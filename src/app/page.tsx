8cd70830dcdde4a2292e7babfa3baea70e101a69
Eduardoooxd <ecouto93@gmail.com>
2024-06-09 15:18:32 +0100

    style: reducing gap between sections on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Skills from '@/components/skills';
 
 export default function Home() {
   return (
-    <main className="flex h-full w-full grow flex-col items-center justify-center gap-12 px-4 py-8 sm:gap-40 sm:pt-36">
+    <main className="flex h-full w-full grow flex-col items-center justify-center gap-12 px-4 py-8 sm:gap-32 sm:pt-36">
       <div className="bg-background-workout absolute right-[11rem] top-[-6rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full bg-[#fbe2e3] blur-[10rem] dark:bg-[#946263] sm:w-[68.75rem]"></div>
 
       <div className="bg-background-workout absolute -top-4 left-[-35rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-[#dbd7fb] blur-[10rem] dark:bg-[#676394] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
