cb679f5d39c4035b765a1022f9a7345eac24893a
Eduardoooxd <ecouto93@gmail.com>
2024-06-09 16:27:39 +0100

    fix: fix error messages on env file

--- src/lib/environment.ts
+++ src/lib/environment.ts
@@ -1,22 +1,22 @@
 import { z } from 'zod';
 
 const environmentVariablesSchema = z.object({
+  RESEND_API_KEY: z.string().min(1),
+  RESEND_FROM_EMAIL: z.string().min(1),
+  RESEND_TO_EMAIL: z.string().min(1),
   NODE_ENV: z
     .string()
     .min(1)
     .refine((value) => ['development', 'production'].includes(value), {
       message: 'NODE_ENV must be either development or production',
     }),
-  RESEND_API_KEY: z.string().min(1),
-  RESEND_FROM_EMAIL: z.string().min(1),
-  RESEND_TO_EMAIL: z.string().min(1),
 });
 
 export const environmentVariables = () => {
   const parsedEnvironmentVariables = environmentVariablesSchema.safeParse(process.env);
 
   if (!parsedEnvironmentVariables.success) {
-    throw new Error(parsedEnvironmentVariables.error.errors.join(', '));
+    throw new Error(parsedEnvironmentVariables.error.message);
   }
 
   return parsedEnvironmentVariables.data;
