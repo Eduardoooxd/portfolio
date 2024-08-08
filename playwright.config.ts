5d098dc07682d1dcc841592e268d58125b507680
Eduardoooxd <ecouto93@gmail.com>
2024-08-08 23:36:47 +0100

    ci: fixing playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -75,7 +75,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'pnpm dev',
-    url: 'http://127.0.0.1:3000',
+    port: 3000,
     reuseExistingServer: !process.env.CI,
   },
 });
